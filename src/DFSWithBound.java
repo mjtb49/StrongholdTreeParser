83d719bb0df669eaf1fa497c21462681c74a29cf
Matthew Bolan <mjb6117@rit.edu>
2020-12-07 20:22:13 -0500

    Added permutations stuff

--- src/DFSWithBound.java
+++ src/DFSWithBound.java
@@ -2,17 +2,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class DFSWithBound {
-    public static int exploreStrongholdBranch(Node n, int maxDepth, int numRooms, int depth) {
+    public static int exploreStrongholdBranchRandomOrder(Node n, int maxDepth, int numRooms, int depth) {
         //no cost and no portal because there wasn't a room
         if (n == null)
-            return -1;
+            return -2;
         //this is the portal room
         if (n.getName().equals("PortalRoom")) {
             return numRooms;
         }
         //we peeked into the room 1 past max depth, so doesn't take travel time
         if (depth > maxDepth)
-            return -1;
+            return -2;
         //ok it is a real room we are in
         numRooms++;
         ArrayList<Node> c = n.getChildren();
@@ -20,15 +20,73 @@ public class DFSWithBound {
         for (Node child:c) {
             if (child !=  null)
                 if (child.getName().equals("PortalRoom"))
-                    return exploreStrongholdBranch(child, maxDepth, numRooms, depth + 1);
+                    return exploreStrongholdBranchRandomOrder(child, maxDepth, numRooms, depth + 1);
         }
         //check children in random order
         Collections.shuffle(c);
         for (Node child: c) {
-            int i = exploreStrongholdBranch(child, maxDepth, numRooms, depth + 1);
+            int i = exploreStrongholdBranchRandomOrder(child, maxDepth, numRooms, depth + 1);
              if (i == -1)
                  numRooms++;
-             else return i;
+             else if (i > 0) return i;
+        }
+        return -1;
+    }
+
+    public static int exploreStrongholdBranchSetOrder(Node n, int maxDepth, int numRooms, int depth, int[] fiveWayOrder, int[] corridorOrder, int[] squareRoomOrder) {
+        //no cost and no portal because there wasn't a room
+        if (n == null)
+            return -2;
+        //this is the portal room
+        if (n.getName().equals("PortalRoom")) {
+            return numRooms;
+        }
+        //we peeked into the room 1 past max depth, so doesn't take travel time
+        if (depth > maxDepth)
+            return -2;
+        //ok it is a real room we are in
+        numRooms++;
+        ArrayList<Node> c = n.getChildren();
+        //head to the portal room cuz I like that
+        for (Node child:c) {
+            if (child !=  null)
+                if (child.getName().equals("PortalRoom"))
+                    return exploreStrongholdBranchSetOrder(child, maxDepth, numRooms, depth + 1, fiveWayOrder, corridorOrder, squareRoomOrder);
+        }
+
+        switch (n.getName()) {
+            case "FiveWayCrossing":
+                for (int room = 0; room < 5; room++) {
+                    int i = exploreStrongholdBranchSetOrder(c.get(fiveWayOrder[room]), maxDepth, numRooms, depth + 1, fiveWayOrder, corridorOrder, squareRoomOrder);
+                    if (i == -1)
+                        numRooms++;
+                    else if (i > 0) return i;
+                }
+                break;
+            case "SquareRoom":
+                for (int room = 0; room < 3; room++) {
+                    int i = exploreStrongholdBranchSetOrder(c.get(squareRoomOrder[room]), maxDepth, numRooms, depth + 1, fiveWayOrder, corridorOrder, squareRoomOrder);
+                    if (i == -1)
+                        numRooms++;
+                    else if (i > 0) return i;
+                }
+                break;
+            case "Corridor":
+                for (int room = 0; room < 3; room++) {
+                    int i = exploreStrongholdBranchSetOrder(c.get(corridorOrder[room]), maxDepth, numRooms, depth + 1, fiveWayOrder, corridorOrder, squareRoomOrder);
+                    if (i == -1)
+                        numRooms++;
+                    else if (i > 0) return i;
+                }
+                break;
+            default:
+                for (Node child : c) {
+                    int i = exploreStrongholdBranchSetOrder(child, maxDepth, numRooms, depth + 1, fiveWayOrder, corridorOrder, squareRoomOrder);
+                    if (i == -1)
+                        numRooms++;
+                    else if (i > 0) return i;
+                }
+                break;
         }
         return -1;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,68 @@
+import java.io.BufferedWriter;
 import java.io.File;
-import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Scanner;
-
 
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File(args[0]);
-        StrongholdParser parser = new StrongholdParser(file);
-        //System.out.println(parser.getNextStronghold());
-        HashMap<String, Integer> numPieces;
-        HashMap<String, Integer> numPortals;
-        int total = 0;
-        int successes = 0;
-        int[] numRoomsExplored = new int[100];
-        while (parser.hasNextStronghold()) {
-            Node start = parser.getNextStronghold();
-            int i = DFSWithBound.exploreStrongholdBranch(start,7,0,0);
-            total++;
-            if (i != -1) {
-                successes++;
-                numRoomsExplored[i]++;
-            }
+        PermIterator fPerms = new PermIterator(5);
+        PermIterator cPerms = new PermIterator(3);
+        PermIterator sPerms = new PermIterator(3);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(new File("output.txt")));
+        int[] f = {0,4,1,3,2}; //default values for if I comment out the loops
+        int[] c = {0,1,2};
+        int[] s = {0,1,2};
+       while (fPerms.hasNext()) {
+          f = fPerms.next();
+           while (cPerms.hasNext()) {
+               c = cPerms.next();
+               while (sPerms.hasNext()) {
+                   s = sPerms.next();
+                    File file = new File(args[0]);
+                    StrongholdParser parser = new StrongholdParser(file);
+                    //System.out.println(parser.getNextStronghold());
+                    HashMap<String, Integer> numPieces;
+                    HashMap<String, Integer> numPortals;
+                    int total = 0;
+                    int successes = 0;
+                    int[] numRoomsExplored = new int[100];
+                    int numChecked = 0;
+                    while (parser.hasNextStronghold() && numChecked < 1000) {
+                        numChecked++;
+                        Node start = parser.getNextStronghold();
+                        //int i = DFSWithBound.exploreStrongholdBranchRandomOrder(start, 7, 0, 0);
+                        int i = DFSWithBound.exploreStrongholdBranchSetOrder(start, 7, 0, 0, f, c, s);
+                        total++;
+                        if (i != -1) {
+                            successes++;
+                            numRoomsExplored[i]++;
+                        }
+                    }
+                    String str = Arrays.toString(f)+" " +Arrays.toString(c)+" " +Arrays.toString(s)+" ";
+                    str += successes+" ";
+                    str += total+" ";
+                    double last = 0.0;
+                   StringBuilder strBuilder = new StringBuilder(str);
+                   for (int i : numRoomsExplored) {
+                        last += i / (double) successes;
+                        strBuilder.append(last).append(" ");
+                    }
+                   str = strBuilder.toString();
+                   str += "\n";
+                   writer.write(str);
+               }
+               sPerms = new PermIterator(3);
+           }
+           cPerms = new PermIterator(3);
         }
-        System.out.println(successes);
-        System.out.println(total);
-        for (int i:numRoomsExplored) {
-            System.out.println(i/(float) successes);
+       writer.flush();
+       writer.close();
+        } catch (IOException exception) {
+            System.err.println(exception.getMessage());
         }
     }
 }
